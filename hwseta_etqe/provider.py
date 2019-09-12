21383d922b7cf3254a2ab537b062c56be9ea432d
ap-systems <dylanb@ap-acounting.co.za>
2019-09-12 10:32:55 +0200

    added debugs to find related reg object in assessments

--- hwseta_etqe/provider.py
+++ hwseta_etqe/provider.py
@@ -10471,7 +10471,7 @@ class provider_assessment(models.Model):
 							learner.learner_qualification_ids = reg_qual_line
 							if learner.citizen_resident_status_code in ['dual', 'PR', 'sa']:
 								learner_reg = self.env['learner.registration'].search(
-									[('identification_id_id', '=', learner.identification_id),
+									[('identification_id', '=', learner.identification_id),
 									 ('learner_qualification_ids.batch_id','=',self.batch_id.id)])
 								dbg(learner_reg)
 							elif learner.citizen_resident_status_code in ['other', 'unknown']:
